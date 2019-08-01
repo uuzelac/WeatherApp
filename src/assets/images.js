683ffce15474b3a254d0e0b10a61ac618afd57fd
Uroš Uzelac <uzuros@gmail.com>
2019-08-01 08:54:21 +0200

    Added image resources

---
+++ src/assets/images.js
@@ -0,0 +1,22 @@
+const images = {
+    clear_sky_lg: require('./img/clear_sky_lg@2x.png'),
+    clear_sky_md: require('./img/clear_sky_md@2x.png'),
+    clear_sky_sm: require('./img/clear_sky_sm@2x.png'),
+    cloudy_lg: require('./img/cloudy_lg@2x.png'),
+    cloudy_md: require('./img/cloudy_md@2x.png'),
+    cloudy_sm: require('./img/cloudy_sm@2x.png'),
+    mostly_cloudy_lg: require('./img/mostly_cloudy_lg@2x.png'),
+    mostly_cloudy_md: require('./img/mostly_cloudy_md@2x.png'),
+    mostly_cloudy_sm: require('./img/mostly_cloudy_sm@2x.png'),
+    partly_sunny_lg: require('./img/partly_sunny_lg@2x.png'),
+    partly_sunny_md: require('./img/partly_sunny_md@2x.png'),
+    partly_sunny_sm: require('./img/partly_sunny_sm@2x.png'),
+    rain_lg: require('./img/rain_lg@2x.png'),
+    rain_md: require('./img/rain_md@2x.png'),
+    rain_sm: require('./img/rain_sm@2x.png'),
+    sunny_lg: require('./img/sunny_lg@2x.png'),
+    sunny_md: require('./img/sunny_md@2x.png'),
+    sunny_sm: require('./img/sunny_sm@2x.png')
+};
+
+export default images;
