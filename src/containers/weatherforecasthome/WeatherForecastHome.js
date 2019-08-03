b9d2467d6db670f4168cdcbb8685b94bf8c8ae71
Uroš Uzelac <uzuros@gmail.com>
2019-08-03 18:27:32 +0200

    Main details for home layout added

--- src/containers/weatherforecasthome/WeatherForecastHome.js
+++ src/containers/weatherforecasthome/WeatherForecastHome.js
@@ -1,10 +1,81 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import { colors } from '../../styles/base';
+import { styles } from './style';
+import images from '../../assets/images';
+import Separator from '../../components/separator/Separator';
 
 class WeatherForecastHome extends Component {
+    static navigationOptions = {
+        title: 'Weather Forecast',
+        headerStyle: {
+            backgroundColor: colors.headerBackground,
+        },
+        headerTintColor: 'white',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+        },
+    };
+
+    openHourlyViewScreen = () => {
+        this.props.navigation.navigate('Details', {
+            content: this.props
+        });
+    }
+
     render() {
         return (
-            <Text>WeatherForecastHome</Text>
+            <SafeAreaView>
+                <View style={styles.container}>
+                    <View style={styles.selectedContainerDetails}>
+                        <View style={styles.selectedContainerPartOne}>
+                            <Text style={styles.selectedContainerTitleText}>TODAY,AUGUST 9</Text>
+                            <View style={styles.selectedContainerDetailsInfo}>
+                                <View style={styles.selectedContainerDetailsInfoLeft}>
+                                    <View style={styles.selectedContainerTemp}>
+                                        <Text style={styles.selectedContainerTempMaxText}>32°</Text>
+                                        <Text style={styles.selectedContainerTempMinText}>/ 19°</Text>
+                                    </View>
+                                    <Text style={styles.selectedContainerInfoText}>Partly sunny, hot and humid</Text>
+                                </View>
+                                <Image style={styles.selectedContainerDetailsInfoRight} source={images.partly_sunny_lg} />
+                            </View>
+                        </View>
+                        <Separator />
+                        <View style={styles.selectedContainerPartTwo}>
+                            <View style={styles.selectedContainerDescription}>
+                                <View style={styles.selectedContainerDescriptionLeft}>
+                                    <Text style={styles.selectedContainerDescriptionText}>RealFeel: 34°</Text>
+                                    <Text style={styles.selectedContainerDescriptionText}>Precipitation: 1%</Text>
+                                    <Text style={styles.selectedContainerDescriptionText}>Max UV Index: 8</Text>
+                                    <Text style={styles.selectedContainerDescriptionText}>Rain: 0mm</Text>
+                                </View>
+                                <View style={styles.selectedContainerDescriptionRight}>
+                                    <Text style={styles.selectedContainerDescriptionGreyedText}>Sunrise:
+                                        <Text style={styles.selectedContainerDescriptionText}> 5:34 AM</Text>
+                                    </Text>
+                                    <Text style={styles.selectedContainerDescriptionGreyedText}>Sunset:
+                                        <Text style={styles.selectedContainerDescriptionText}> 7:53 PM</Text>
+                                    </Text>
+                                </View>
+                            </View>
+                        </View>
+                        <Separator />
+                        <View style={styles.selectedContainerPartThree}>
+                            <TouchableOpacity onPress={this.openHourlyViewScreen}>
+                                <View style={styles.selectedContainerButtonWrapper}>
+                                    <Text style={styles.selectedContainerButton}>SEE HOURLY</Text>
+                                    <Text style={styles.selectedContainerButtonArrow}>&#10140;</Text>
+                                </View>
+                            </TouchableOpacity>
+
+                        </View>
+                    </View>
+                    <View style={styles.selectedContainerCardsView}>
+                        <Text style={styles.selectedContainerTitleText}>DAY CARDS</Text>
+                    </View>
+                </View>
+            </SafeAreaView>
         );
     }
 }
---
+++ src/containers/weatherforecasthome/style.js
@@ -0,0 +1,109 @@
+import { StyleSheet } from 'react-native';
+import { colors, fontSize, margin, padding } from '../../styles/base';
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.greyLight,
+        height: '100%',
+    },
+    selectedContainerDetails: {
+        flex: 3,
+        margin: margin.sm,
+        backgroundColor: 'white',
+    },
+    selectedContainerPartOne: {
+        flex: 4,
+        justifyContent: 'center',
+        paddingTop: padding.sm,
+        paddingBottom: padding.sm,
+        paddingStart: padding.md,
+        paddingEnd: padding.md,
+    },
+    selectedContainerPartTwo: {
+        flex: 3,
+        justifyContent: 'center',
+        paddingTop: padding.sm,
+        paddingBottom: padding.sm,
+        paddingStart: padding.md,
+        paddingEnd: padding.md,
+    },
+    selectedContainerPartThree: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingTop: padding.sm,
+        paddingBottom: padding.sm,
+        paddingStart: padding.md,
+        paddingEnd: padding.md,
+    },
+    selectedContainerTitleText: {
+        color: colors.headerTitle,
+        fontWeight: 'bold'
+    },
+    selectedContainerDetailsInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    selectedContainerDetailsInfoLeft: {
+    },
+    selectedContainerDetailsInfoRight: {
+        width: 120,
+        height: 120,
+    },
+    selectedContainerTemp: {
+        flexDirection: 'row',
+    },
+    selectedContainerTempMaxText: {
+        color: 'black',
+        fontSize: fontSize.maxTemp
+    },
+    selectedContainerTempMinText: {
+        paddingTop: padding.sm,
+        color: colors.greyDark,
+        fontSize: fontSize.minTemp
+    },
+    selectedContainerInfoText: {
+        color: colors.greyDark,
+        fontSize: fontSize.md,
+        width: 120
+    },
+    selectedContainerDescription: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    selectedContainerDescriptionLeft: {
+        alignItems: 'flex-start'
+    },
+    selectedContainerDescriptionRight: {
+        alignItems: 'flex-end'
+    },
+    selectedContainerDescriptionText: {
+        marginBottom: margin.xxs,
+        fontSize: fontSize.md,
+        color: 'black'
+    },
+    selectedContainerDescriptionGreyedText: {
+        fontSize: fontSize.md,
+        marginBottom: margin.xxs,
+        color: colors.greyDark
+    },
+    selectedContainerCardsView: {
+        flex: 2,
+        marginBottom: margin.sm,
+        marginStart: margin.sm,
+        marginEnd: margin.sm,
+        backgroundColor: 'white',
+    },
+    selectedContainerButtonWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    selectedContainerButton: {
+        fontSize: fontSize.md,
+        color: colors.headerBackground,
+        fontWeight: 'bold',
+        marginEnd: margin.xs
+    },
+    selectedContainerButtonArrow: {
+        color: colors.headerBackground,
+    },
+});
