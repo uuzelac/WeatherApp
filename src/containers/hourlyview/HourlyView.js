a294b9abe43a3c8f2f91378a883a12e1de0e012d
Uroš Uzelac <uzuros@gmail.com>
2019-08-03 18:28:22 +0200

    Added navigationOptions for HourlyView Screen

--- src/containers/hourlyview/HourlyView.js
+++ src/containers/hourlyview/HourlyView.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { colors } from '../../styles/base';
 
 class HourlyView extends Component {
+    static navigationOptions = {
+        title: 'Details',
+        headerStyle: {
+            backgroundColor: colors.headerBackground,
+        },
+        headerTintColor: 'white',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+        },
+    };
     render() {
         return (
             <Text>HourlyView</Text>
