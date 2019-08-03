0f719d011b5b69cedef11faf3900fba45ca3bbe9
Uroš Uzelac <uzuros@gmail.com>
2019-08-03 10:22:22 +0200

    Added basic project structure

---
+++ src/containers/hourlyview/HourlyView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class HourlyView extends Component {
+    render() {
+        return (
+            <Text>HourlyView</Text>
+        );
+    }
+}
+
+export default HourlyView;
---
+++ src/containers/weatherforecasthome/WeatherForecastHome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class WeatherForecastHome extends Component {
+    render() {
+        return (
+            <Text>WeatherForecastHome</Text>
+        );
+    }
+}
+
+export default WeatherForecastHome;
