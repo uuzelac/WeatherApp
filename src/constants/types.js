184930ad835a1a141b1bd8aff2ef52c26e4bf2bd
Uroš Uzelac <uzuros@gmail.com>
2019-08-04 18:52:14 +0200

    Added new types

---
+++ src/constants/types.js
@@ -0,0 +1,13 @@
+export const GET_WEATHER_DATA_SUCCESS = 'get_weather_data_success';
+export const GET_WEATHER_DATA_FAIL = 'get_weather_data_fail';
+
+export const PARTLY_SUNNY = 'partly_sunny';
+export const RAIN = 'rain';
+export const SUNNY = 'sunny';
+export const MOSTLY_CLOUDY = 'mostly_cloudy';
+export const CLEAR_SKY = 'clear_sky';
+export const CLOUDY = 'cloudy';
+
+export const SIZE_SMALL = 'sm';
+export const SIZE_MEDIUM = 'md';
+export const SIZE_LARGE = 'lg';
