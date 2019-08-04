da975998e9551fb1a89b945b6d28a9bc178fa4ac
Uroš Uzelac <uzuros@gmail.com>
2019-08-04 18:51:25 +0200

    Added provider and store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
 import RootRouter from './RootRouter';
+import store from './store';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={{ display: 'flex', flex: 1 }}>
-        <RootRouter />
-      </View>
+      <Provider store={store}>
+        <View style={{ display: 'flex', flex: 1 }}>
+          <RootRouter />
+        </View>
+      </Provider>
     );
   }
 }
