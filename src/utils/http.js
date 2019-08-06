99dd85c754bfcf8f65f7e68c1c3894068ebaf4c1
Uroš Uzelac <uzuros@gmail.com>
2019-08-06 22:58:14 +0200

    Added loading actions to axios interceptors

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,19 +1,24 @@
 import axios from 'axios';
+import store from '../store';
+import { loadingStart, loadingStop } from '../actions/LoadingAction';
 
 const http = axios.create();
 
 http.interceptors.request.use(request => {
-    console.log('REQ: ', request);
+    // console.log('REQ: ', request);
+    store.dispatch(loadingStart());
     return request;
 });
 
 http.interceptors.response.use(response => {
-        console.log('RES: ', response);
-        return response;
-    }, error => {
-        console.log('ERR:', error);
-        throw error;
-    }
+    // console.log('RES: ', response);
+    store.dispatch(loadingStop());
+    return response;
+}, error => {
+    // console.log('ERR:', error);
+    store.dispatch(loadingStop());
+    throw error;
+}
 );
 
 export default http;
