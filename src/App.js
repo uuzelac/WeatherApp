f2d4449b69850cccc48d34f0e586adf86eaf0cc0
Uroš Uzelac <uzuros@gmail.com>
2019-08-06 22:59:16 +0200

    Added Spinner above RootRouter in App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import RootRouter from './RootRouter';
 import store from './store';
+import Spinner from './components/spinner/Spinner';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <View style={{ display: 'flex', flex: 1 }}>
+          <Spinner />
           <RootRouter />
         </View>
       </Provider>
